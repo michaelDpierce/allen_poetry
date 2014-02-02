344084bba04eaf7030183fb4f03187606976bcce
Michael Pierce <michael@affektive.com>
2014-02-01 21:24:13 -0700

    cosmetic changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Allen Poetry"
+    base_title = "Pierce Poetry"
     if page_title.empty?
       base_title
     else
